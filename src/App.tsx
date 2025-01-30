19ad42af63e3822f3b0eadc61e3570ea6042d1ee
Elif Balci <mrselifbalci@gmail.com>
2025-01-30 23:00:47 +0000

    fix imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,7 @@
-import React, { useState } from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Link,
-  useLocation,
-} from "react-router-dom";
-import { Box, Typography } from "@mui/material";
-import "./App.css";
-import TimeAndDate from "./TimeAndDate";
-import TimeGrid from "./TimeGrid";
-import GunlukSureler from "./GunlukSureler";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Cuzler from "./Cuzler";
 
 const App: React.FC = () => {
-  const [selectedIslamicDate, setSelectedIslamicDate] = useState<string>("");
-
-  const handleDateChange = (date: string) => {
-    setSelectedIslamicDate(date);
-  };
-
-  const NavLink = ({
-    to,
-    children,
-  }: {
-    to: string;
-    children: React.ReactNode;
-  }) => {
-    const location = useLocation();
-    const isActive = location.pathname === to;
-
-    return (
-      <Link to={to} style={{ textDecoration: "none" }}>
-        <Typography
-          variant="h6"
-          sx={{
-            cursor: "pointer",
-            textDecoration: isActive ? "underline" : "none",
-          }}
-        >
-          {children}
-        </Typography>
-      </Link>
-    );
-  };
-
   return (
     <Router>
       <Routes>
