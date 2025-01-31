f8556256da3bef8d7322700f5a14b0351fc5d557
Elif Balci <mrselifbalci@gmail.com>
2025-01-31 10:57:58 +0000

    4

--- src/Cuzler.tsx
+++ src/Cuzler.tsx
@@ -69,7 +69,7 @@ const Cuzler: React.FC = () => {
         );
 
         setCuzlers(sortedData);
-        filterByHatim(3, sortedData);
+        filterByHatim(4, sortedData);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
